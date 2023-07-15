5d022ab329a761f7f1f80fb5c4b024884d89cad4
Redbone <baruaesau254@gmail.com>
2023-07-15 22:40:07 +0300

    added another student

--- student.java
+++ student.java
@@ -6,10 +6,14 @@ public class student {
 		stud s1 = new stud("Mark", 200300, 23);
 		stud s2 = new stud("John", 211300, 19);
 		stud s3 = new stud("Luke", 222300, 25);
+		stud s4 = new stud("Brian", 222333, 27);
 		
 		stud studentArr[] = new stud[stud.getStudNumber()];	
 		
-		System.out.println(studentArr[0]);
+		studentArr[0] = s1;
+		studentArr[1] = s2;
+		studentArr[2] = s3;
+		studentArr[3] = s4;
 		
 		
 		for(int indexCount = 0; indexCount <= stud.getStudNumber(); indexCount++) {
